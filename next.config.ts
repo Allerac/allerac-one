6f723118b2eaa59366b8ddff20b26a738ef599c7
gianclaudiocarella <gianclaudio.gc@gmail.com>
2026-03-23 11:31:30 +0100

    fixing build

--- next.config.ts
+++ next.config.ts
@@ -11,32 +11,30 @@ const nextConfig: NextConfig = {
   // Next.js standalone file tracing misses .mjs files from ESM-only packages
   // that are transitive deps of oidc-provider. Include them explicitly so
   // they are copied into .next/standalone/node_modules.
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**': [
-        './node_modules/oidc-provider/**',
-        './node_modules/generator-function/**',
-        './node_modules/jose/**',
-        './node_modules/nanoid/**',
-        './node_modules/oidc-token-hash/**',
-        './node_modules/got/**',
-        './node_modules/@sindresorhus/**',
-        './node_modules/@szmarczak/**',
-        './node_modules/cacheable-lookup/**',
-        './node_modules/cacheable-request/**',
-        './node_modules/decompress-response/**',
-        './node_modules/form-data-encoder/**',
-        './node_modules/get-stream/**',
-        './node_modules/http2-wrapper/**',
-        './node_modules/lowercase-keys/**',
-        './node_modules/mimic-response/**',
-        './node_modules/normalize-url/**',
-        './node_modules/p-cancelable/**',
-        './node_modules/quick-lru/**',
-        './node_modules/responselike/**',
-        './node_modules/eta/**',
-      ],
-    },
+  outputFileTracingIncludes: {
+    '/**': [
+      './node_modules/oidc-provider/**',
+      './node_modules/generator-function/**',
+      './node_modules/jose/**',
+      './node_modules/nanoid/**',
+      './node_modules/oidc-token-hash/**',
+      './node_modules/got/**',
+      './node_modules/@sindresorhus/**',
+      './node_modules/@szmarczak/**',
+      './node_modules/cacheable-lookup/**',
+      './node_modules/cacheable-request/**',
+      './node_modules/decompress-response/**',
+      './node_modules/form-data-encoder/**',
+      './node_modules/get-stream/**',
+      './node_modules/http2-wrapper/**',
+      './node_modules/lowercase-keys/**',
+      './node_modules/mimic-response/**',
+      './node_modules/normalize-url/**',
+      './node_modules/p-cancelable/**',
+      './node_modules/quick-lru/**',
+      './node_modules/responselike/**',
+      './node_modules/eta/**',
+    ],
   },
 };
 
